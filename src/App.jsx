3c8debd0c03f8d8e7b74a904b9e1338c20773c44
MatthieuMosch <mmosch@gmail.com>
2025-02-23 13:10:42 +0100

    eerste opzet: lijst met de common names van de landen

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,35 @@
 import './App.css';
+import {useState} from "react";
+import axios from "axios";
 
 function App() {
 
+    const [countryData, setCountryData] = useState([])
+
+    async function getCountryData() {
+        try {
+            const uri = "https://restcountries.com/v3.1/all"
+            const newCountryData = await axios.get(uri)
+            setCountryData(newCountryData.data);
+            console.log(newCountryData.data);
+        } catch (e) {
+            console.error("foutje:" + e.message);
+        } finally {
+
+        }
+    }
+
     return (
         <>
-            Maak je applicatie hier!
+            <h1>Landen informatie</h1>
+            <button onClick={getCountryData}>get data</button>
+            <ul>
+                {countryData.length > 0 && countryData.map((country) => (
+                    <li key={country.name.common}>
+                        {country.name.common}
+                    </li>
+                ))}
+            </ul>
         </>
     )
 }
